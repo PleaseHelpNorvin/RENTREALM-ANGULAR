35846304a8906f13b6bc7b9e225816856c0bd71f
PleaseHelpNorvin <140196018+PleaseHelpNorvin@users.noreply.github.com>
2024-12-10 18:46:20 +0800

    hover

--- src/app/shared/admin-layout/navbar/navbar.component.ts
+++ src/app/shared/admin-layout/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { MatIconModule  } from '@angular/material/icon';
+import { MatIcon  } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIconModule ,CommonModule,MatToolbarModule],
+  imports: [MatIcon ,CommonModule,MatToolbarModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
